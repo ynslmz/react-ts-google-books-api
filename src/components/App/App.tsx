ebc618cb1b43214dc188d680716ca6ff477f7901
Yunus ÖLMEZ <yns4832@gmail.com>
2023-02-23 19:14:12 +0100

    Loading animation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,47 +7,74 @@ import "./app.scss";
 import { useEffect, useState } from "react";
 import { Book } from "../../types/book";
 import BookList from "../BookList/bookList";
+import { Paginator } from "../Paginator/paginator";
+import { Loading } from "../Loading/loading";
 
 function App() {
+  const [loading, SetLoading] = useState(false);
   const [sorting, setSorting] = useState("relevance");
   const [query, setQuery] = useState("");
   const [books, setBooks] = useState<Book[]>([]);
+  const [totalItems, setTotalItems] = useState(0);
+  const [startIndex, setStartIndex] = useState(0);
 
   useEffect(() => {
     async function fetchData() {
       try {
         if (query.length > 0) {
+          SetLoading(true);
           const response = await search({
             query: query,
-            startIndex: "0",
+            startIndex: startIndex,
             orderBy: sorting,
-            pageSize: "10",
+            pageSize: 10,
           });
-          setBooks(response.items);
+          setBooks(!!response.items ? response.items : []);
+          !!response.totalItems && setTotalItems(response.totalItems);
         }
       } catch (error) {
         console.error(error);
+      } finally {
+        SetLoading(false);
       }
     }
     fetchData();
-  }, [query, sorting]);
+  }, [query, sorting, startIndex]);
+
+  function handleSearch(searchText: string): void {
+    setQuery(searchText);
+    setStartIndex(0);
+  }
+
+  function handlePageChange(newIndex: number): void {
+    setStartIndex(newIndex);
+  }
+
+  function handleSorting(sorting: string): void {
+    setSorting(sorting);
+    setStartIndex(0);
+  }
 
   return (
     <div className="app">
       <Header small={books.length > 0} />
-      <Search
-        onSearch={(searchText) => {
-          setQuery(searchText);
-        }}
-      />
-      <FilterBar
-        onSortChange={(sorting) => {
-          setSorting(sorting);
-        }}
-      />
-      <div className="app-body">
-        <BookList books={books} />
-      </div>
+      <Search onSearch={handleSearch} />
+      <FilterBar onSortChange={handleSorting} />
+      {!loading && (
+        <div className="app-body">
+          <BookList books={books} />
+          <div className="pagination">
+            {totalItems > 0 && (
+              <Paginator
+                totalItems={totalItems}
+                startIndex={startIndex}
+                onPageChange={handlePageChange}
+              />
+            )}
+          </div>
+        </div>
+      )}
+      {loading && <Loading />}
     </div>
   );
 }
---
+++ src/components/Loading/loading.tsx
@@ -0,0 +1,15 @@
+import "./loading.scss";
+export function Loading() {
+  return (
+    <div className="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
